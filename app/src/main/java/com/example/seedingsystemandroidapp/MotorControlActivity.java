0154f34a974b99d0ed6b039dbbdfcb8a225b7415
Wangshenggui <1785193871@qq.com>
2024-07-01 14:10:22 +0800

    完成RTK信息解析

--- app/src/main/java/com/example/seedingsystemandroidapp/MotorControlActivity.java
+++ app/src/main/java/com/example/seedingsystemandroidapp/MotorControlActivity.java
@@ -21,8 +21,12 @@ import org.json.JSONObject;
 
 public class MotorControlActivity extends AppCompatActivity {
 
-    private TextView RxTextView;
     private TextView SpeedTextView;
+    private TextView altiTextView;
+    private TextView RTKStatusTextView;
+    private TextView RTKHCSDSTextView;
+    private TextView lonTextView;
+    private TextView latTextView;
     private Button SendDataButton;
     private final Handler mainHandler = new Handler(Looper.getMainLooper());
     private WebSocketServiceReceiver receiver;
@@ -40,9 +44,13 @@ public class MotorControlActivity extends AppCompatActivity {
         toolbar.setTitle("电机控制");
         toolbar.setTitleTextAppearance(this, R.style.ToolbarTitleText);
 
-        RxTextView = findViewById(R.id.RxTextView);
         SendDataButton = findViewById(R.id.SendDataButton);
         SpeedTextView = (TextView) findViewById(R.id.SpeedTextView);
+        altiTextView = (TextView) findViewById(R.id.altiTextView);
+        RTKStatusTextView = (TextView) findViewById(R.id.RTKStatusTextView);
+        RTKHCSDSTextView = (TextView) findViewById(R.id.RTKHCSDSTextView);
+        lonTextView = (TextView) findViewById(R.id.lonTextView);
+        latTextView = (TextView) findViewById(R.id.latTextView);
 
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -91,6 +99,27 @@ public class MotorControlActivity extends AppCompatActivity {
                     double speed = Double.parseDouble(data); // 将字符串转换为浮点数
                     String formattedData = String.format("%.3f", speed); // 格式化为三位小数
                     SpeedTextView.setText("速度(km/h):" + formattedData);
+
+                    data = jsonObject.getString("alti");
+                    double alti = Double.parseDouble(data); // 将字符串转换为浮点数
+                    formattedData = String.format("%.2f", alti); // 格式化为三位小数
+                    altiTextView.setText("海拔(m):" + formattedData);
+
+                    data = jsonObject.getString("rtksta");
+                    RTKStatusTextView.setText("RTK状态:" + data);
+
+                    data = jsonObject.getString("HCSDS");
+                    RTKHCSDSTextView.setText("卫星数量:" + data);
+
+                    data = jsonObject.getString("lon");
+                    double lon = Double.parseDouble(data); // 将字符串转换为浮点数
+                    formattedData = String.format("%.10f", lon); // 格式化为三位小数
+                    lonTextView.setText("经度:" + formattedData);
+
+                    data = jsonObject.getString("lat");
+                    double lat = Double.parseDouble(data); // 将字符串转换为浮点数
+                    formattedData = String.format("%.10f", lat); // 格式化为三位小数
+                    latTextView.setText("经度:" + formattedData);
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
