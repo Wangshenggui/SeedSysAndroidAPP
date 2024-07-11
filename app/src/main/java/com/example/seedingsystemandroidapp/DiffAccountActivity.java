88ac0ad789919f6df5e71ac54d16994299267978
Wangshenggui <1785193871@qq.com>
2024-07-11 16:47:27 +0800

    实现差分数据WebSocket的通信

--- app/src/main/java/com/example/seedingsystemandroidapp/DiffAccountActivity.java
+++ app/src/main/java/com/example/seedingsystemandroidapp/DiffAccountActivity.java
@@ -1,5 +1,6 @@
 package com.example.seedingsystemandroidapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
@@ -26,14 +27,22 @@ import java.util.List;
 public class DiffAccountActivity extends AppCompatActivity {
 
     private Spinner spinner;
+    RadioGroup radioGroup1;
+    RadioGroup radioGroup2;
+    CheckBox showPasswordCheckBox;
+    EditText PasswordeditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diff_account);
 
-        RadioGroup radioGroup1 = findViewById(R.id.radioGroup1);
-        RadioGroup radioGroup2 = findViewById(R.id.radioGroup2);
+        radioGroup1 = findViewById(R.id.radioGroup1);
+        radioGroup2 = findViewById(R.id.radioGroup2);
+        showPasswordCheckBox = findViewById(R.id.showPasswordCheckBox);
+        PasswordeditText = findViewById(R.id.PasswordeditText);
+        spinner = findViewById(R.id.spinner);
+
         radioGroup1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
@@ -56,9 +65,7 @@ public class DiffAccountActivity extends AppCompatActivity {
                 }
             }
         });
-
-        CheckBox showPasswordCheckBox = findViewById(R.id.showPasswordCheckBox);
-        EditText PasswordeditText = findViewById(R.id.PasswordeditText);
+        //显示密码
         showPasswordCheckBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
             if (isChecked) {
                 PasswordeditText.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
@@ -68,14 +75,11 @@ public class DiffAccountActivity extends AppCompatActivity {
             PasswordeditText.setSelection(PasswordeditText.length()); // 保持光标在文本末尾
         });
 
-        spinner = findViewById(R.id.spinner);
-
         // 创建一个选项列表（数据源）
         List<String> categories = new ArrayList<>();
         categories.add("选项 1");
         categories.add("选项 2");
         categories.add("选项 3");
-
         // 创建一个适配器（Adapter），用于将数据与Spinner关联起来
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories) {
             @Override
@@ -87,7 +91,6 @@ public class DiffAccountActivity extends AppCompatActivity {
                 textView.setTextSize(20); // 根据需要调整字体大小
                 return view;
             }
-
             @Override
             public View getDropDownView(int position, View convertView, ViewGroup parent) {
                 // 获取默认下拉视图
@@ -98,13 +101,10 @@ public class DiffAccountActivity extends AppCompatActivity {
                 return view;
             }
         };
-
         // 设置下拉列表框的样式
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
         // 将适配器设置到Spinner
         spinner.setAdapter(dataAdapter);
-
         // 设置Spinner的选择监听器
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -119,5 +119,9 @@ public class DiffAccountActivity extends AppCompatActivity {
                 // 当没有选项被选择时触发
             }
         });
+
+        Intent intent = new Intent("SendWebSocketMessage");
+        intent.putExtra("message", "这是消息");
+        sendBroadcast(intent);
     }
 }
